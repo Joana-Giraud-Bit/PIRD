08d56327e6b781d2560c28f1241656c8fe1cdff9
Joana-Giraud-Bit <153189003+Joana-Giraud-Bit@users.noreply.github.com>
2024-01-22 14:14:53 +0100

    Add files via upload

--- Code/Correlation analysis of apartment to themselves.py
+++ Code/Correlation analysis of apartment to themselves.py
@@ -46,15 +46,15 @@ print(df_12_corr)
 df3=df1.resample('H').mean()
 df4=df2.resample('H').mean()
 
-print(df3)
-print(df4)
+print(df3.shape)
+print(df4.shape)
 
 df4=df4.drop('2020-12-30 13:00:00')
 df4=df4.drop('2020-12-30 14:00:00')
 df4=df4.drop('2020-12-30 15:00:00')
 df4=df4.drop('2020-12-30 16:00:00')
 
-print(df4)
+print(df4.shape)
 
 df_34_corr =df3.corrwith(df4.set_axis(df3.index, axis='index', copy=False)) # Correlation matrice of df1h with df2 changing the index name with the index name of dh1 to compare them
 print(df_34_corr)
@@ -63,8 +63,8 @@ print(df_34_corr)
 df5=df1.resample('D').mean()
 df6=df2.resample('D').mean()
 
-print(df5)
-print(df6)
+print(df5.shape)
+print(df6.shape)
 
 df5=df5.drop('2020-12-16')
 
@@ -75,8 +75,8 @@ print(df_56_corr)
 df7=df1.resample('5D').mean()
 df8=df2.resample('5D').mean()
 
-print(df7)
-print(df8)
+print(df7.shape)
+print(df8.shape)
 
 df7=df7.drop('2020-12-16')
 
@@ -88,8 +88,8 @@ print(df_78_corr)
 
 df9=df['2020-12-01 00:00:00' : '2020-12-31 00:00:00']
 df10=df['2021-01-01 00:00:00' : '2021-01-30 13:00:00']
-print(df9)
-print(df10)
+print(df9.shape)
+print(df10.shape)
 
 df_910_corr =df9.corrwith(df10.set_axis(df9.index, axis='index', copy=False)) # Correlation matrice of df1h with df2 changing the index name with the index name of dh1 to compare them
 print(df_910_corr)
@@ -110,11 +110,11 @@ print(df_1112_corr)
 df13=df9.resample('10D').mean()
 df14=df10.resample('10D').mean()
 
-print(df13)
-print(df14)
+print(df13.shape)
+print(df14.shape)
 
 df13=df13.drop('2020-12-31')
-print(df13)
+print(df13.shape)
 
 df_1415_corr =df13.corrwith(df14.set_axis(df13.index, axis='index', copy=False)) # Correlation matrice of df1h with df2 changing the index name with the index name of dh1 to compare them
 print(df_1415_corr)
@@ -123,8 +123,8 @@ print(df_1415_corr)
 
 df15=df['2020-12-01 00:00:00' : '2021-02-28 00:00:00']
 df16=df['2021-03-01 00:00:00' : '2021-05-28 04:00:00']
-print(df15)
-print(df16)
+print(df15.shape)
+print(df16.shape)
 
 
 df_1516_corr =df15.corrwith(df16.set_axis(df15.index, axis='index', copy=False)) # Correlation matrice of df1h with df2 changing the index name with the index name of dh1 to compare them
@@ -132,8 +132,8 @@ print(df_1516_corr)
 
 df17=df15.resample('H').mean()
 df18=df16.resample('H').mean()
-print(df17)
-print(df18)
+print(df17.shape)
+print(df18.shape)
 
 df17=df17.drop('2020-12-01 12:00:00')
 df17=df17.drop('2020-12-01 13:00:00')
@@ -151,8 +151,8 @@ print(df_1718_corr)
 
 df19=df15.resample('D').mean()
 df20=df16.resample('D').mean()
-print(df19)
-print(df20)
+print(df19.shape)
+print(df20.shape)
 
 df19=df19.drop('2020-12-01')
 
@@ -163,8 +163,8 @@ print(df_1920_corr)
 
 df21=df15.resample('10D').mean()
 df22=df16.resample('10D').mean()
-print(df19)
-print(df20)
+print(df21.shape)
+print(df22.shape)
 
 
 
@@ -173,8 +173,8 @@ print(df_2122_corr)
 
 df23=df15.resample('M').mean()
 df24=df16.resample('M').mean()
-print(df23)
-print(df24)
+print(df23.shape)
+print(df24.shape)
 
 
 
